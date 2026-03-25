52fbbd76c1c6fe4f52afb4ed65d2a9c6309f50c0
KANIESH RAJ R <kanieshraj29@gmail.com>
2026-03-26 01:07:44 +0530

    Update API base URL to production server

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
     // 👇 Replace this with your BACKEND Render URL!
-    baseURL: 'http://localhost:5000', 
+    baseURL: 'https://ar-boutique-backend-98li.onrender.com/', 
     headers: {
         'Content-Type': 'application/json',
     },
