6b62f86d1aa8b8b692dbbfe3204214031892d72f
KANIESH RAJ R <kanieshraj29@gmail.com>
2026-03-25 23:32:05 +0530

    Update api.js

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
     // 👇 Replace this with your BACKEND Render URL!
-    baseURL: 'https://your-backend-app.onrender.com', 
+    baseURL: 'https://ar-boutique-backends.onrender.com', 
     headers: {
         'Content-Type': 'application/json',
     },
